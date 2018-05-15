b58863bd57a33aaa1d9efc08f9770087a0a49210
tboissel <tboissel@e1r11p15.42.fr>
2018-05-15 12:06:16 +0200

    altitude ok sans variable multi, iso ok, pixels gap marche

--- display.c
+++ display.c
@@ -6,7 +6,7 @@
 /*   By: tboissel <tboissel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/10 11:17:15 by tboissel          #+#    #+#             */
-/*   Updated: 2018/05/14 19:13:43 by tboissel         ###   ########.fr       */
+/*   Updated: 2018/05/15 12:02:11 by tboissel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	ft_prepare_mlx(t_map *map)
 	t_minilibx	mlx;
 	t_pixels	pixels;
 	
-	map->win_height = map->height * 75;
-	map->win_width = map->width * 75;
+	map->win_height = 400 + 50 * (map->height / 2);
+	map->win_width = 400 + 50 * (map->width / 2);
 	mlx.mlx_ptr = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx_ptr, map->win_width, map->win_height, "test");
 	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, map->win_width, map->win_height);
@@ -38,12 +38,15 @@ t_pixels	ft_create_pixel_map(t_map *map)
 
 	line = 0;
 	i = -1;
-	pixels.gap = (map->win_height) / (map->height * 1.4);
+	if (map->win_height > map->win_width)
+		pixels.gap = (map->win_height) / (map->height * 1.7);
+	else
+		pixels.gap = (map->win_width) / (map->width * 1.7);
 	pixels.coord = malloc(sizeof(t_coord) * map->nb_points);
 	while (++i < map->nb_points)
 	{
-		pixels.coord[i].x = 340 + (((1 / sqrt(3))) * ((i % map->width) - line) * pixels.gap) ;
-		pixels.coord[i].y = 10 + ((0.5) * (line + (i % map->width))) * pixels.gap;
+		pixels.coord[i].x = 300 + round(((2 / sqrt(3))) * ((i % map->width) - line) * pixels.gap) ;
+		pixels.coord[i].y = 125 + round(((0.5) * (line + (i % map->width))) * pixels.gap) - map->z[line][i % map->width];
 		if (!((i + 1) % map->width))
 			line++;
 	}
